56539b8aa747dc1bd41ed080989f5e8c34c166d2
Geoff Clifton <geoffreyclifton@gmail.com>
2014-12-16 21:20:36 -0500

    completed challenge 1

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMesageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,11 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMesageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = "To: " + enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
         
         enterMesageTextField.text = ""
         enterMesageTextField.resignFirstResponder()
